32993b01894a21fda1bab959b22193c2115b9472
Nayara Denise Gaspar <nayarag@cooxupe.com.br>
2023-10-26 08:59:13 -0300

    Microsserviço de acesso de veículos

---
+++ ms-acesso/src/main/java/com/nayaragaspar/msacesso/MsAcessoApplication.java
@@ -0,0 +1,13 @@
+package com.nayaragaspar.msacesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAcessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAcessoApplication.class, args);
+	}
+
+}
---
+++ ms-acesso/src/test/java/com/nayaragaspar/msacesso/MsAcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayaragaspar.msacesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
